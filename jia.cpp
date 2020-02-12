38661ebafe91ca4415d02bf61935fba5e0b8f10a
Jiacheng Huang <huangjiacheng0709@outlook.com>
2020-02-12 12:04:54 -0600

    Updated operations and added conditional and loop control statements

--- jia.cpp
+++ jia.cpp
@@ -46,16 +46,13 @@ Jia::Jia(std::vector<std::string> set_expression) {
 void Jia::loop() {
     for (;;) {
 	std::string input;
-	int front_parenthesis_count = 0, back_parenthesis_count = 0;
-	while(std::cin >> input) {
-	    if (input == "(") ++front_parenthesis_count;
-	    if (input == ")") ++back_parenthesis_count;
-	    expression_.push_back(input);
-	    if (front_parenthesis_count == back_parenthesis_count) {
-		for (auto str : execute()) std::cout << str << std::endl;
-		break;
-	    }
-	}
+	std::getline(std::cin, input);
+	std::stringstream input_string_stream(input);
+	expression_.push_back("(");
+	std::string input_string;
+	while (input_string_stream >> input_string) expression_.push_back(input_string);
+	expression_.push_back(")");
+	for (auto str : execute()) std::cout << str << std::endl;
 	expression_.clear();
     }
 }
@@ -142,16 +139,81 @@ std::vector<std::string> Jia::execute() {
 	definitions.insert(std::pair<std::string, Definition>(expression_[2], definition));
 	return {expression_[2]};
     }
+    
     std::vector<std::string> expression = recurse();
     if (expression[1] == "exit") exit(0);
     if (definitions.find(expression[1]) != definitions.end()) {
 	Jia jia_recursion(rewriteExpression(expression));
 	return jia_recursion.execute();
     }
+    if (expression[1] == "if") {
+	if (expression[2] == "true") {
+	    Jia jia_true(split(3));
+	    return jia_true.execute();
+	} else {
+	    Jia jia_false(split(3));
+	    return jia_false.execute();
+	}
+    }
+    if (expression[1] == "while") {
+	while (expression[2] == "true") {
+	    Jia jia_true(split(0));
+	    return jia_true.execute();
+	}
+    }
     if (expression[1] == "+") {
-	double first = std::stod(expression[2]), second = std::stod(expression[3]);
-	std::string str = std::to_string(first + second);
+	double sum = std::stod(expression[2]);
+        for (size_t i = 3; i < expression.size() - 1; ++i) {
+	    decltype(sum) next = std::stod(expression[i]);
+	    sum += next;
+	}
+	std::string str = std::to_string(sum);
 	return {str};
+    } else if (expression[1] == "-") {
+	double sum = std::stod(expression[2]);
+        for (size_t i = 3; i < expression.size() - 1; ++i) {
+	    decltype(sum) next = std::stod(expression[i]);
+	    sum -= next;
+	}
+	std::string str = std::to_string(sum);
+	return {str};
+    } else if (expression[1] == "*") {
+	double sum = std::stod(expression[2]);
+        for (size_t i = 3; i < expression.size() - 1; ++i) {
+	    decltype(sum) next = std::stod(expression[i]);
+	    sum *= next;
+	}
+	std::string str = std::to_string(sum);
+	return {str};
+    } else if (expression[1] == "/") {
+	double sum = std::stod(expression[2]);
+        for (size_t i = 3; i < expression.size() - 1; ++i) {
+	    decltype(sum) next = std::stod(expression[i]);
+	    sum /= next;
+	}
+	std::string str = std::to_string(sum);
+	return {str};
+    } else if (expression[1] == "==") {
+	double first = std::stod(expression[2]);
+        for (size_t i = 3; i < expression.size() - 1; ++i) {
+	    decltype(first) next = std::stod(expression[i]);
+	    if (first != next) return {"false"};
+	}
+	return {"true"};
+    } else if (expression[1] == "<") {
+	double first = std::stod(expression[2]);
+        for (size_t i = 3; i < expression.size() - 1; ++i) {
+	    decltype(first) next = std::stod(expression[i]);
+	    if (first >= next) return {"false"};
+	}
+        return {"true"};
+    } else if (expression[1] == ">") {
+	double first = std::stod(expression[2]);
+        for (size_t i = 3; i < expression.size() - 1; ++i) {
+	    decltype(first) next = std::stod(expression[i]);
+	    if (first <= next) return {"false"};
+	}
+	return {"true"};
     } else if (expression.size() == 3) {
 	return {expression[1]};
     } else if (expression.size() == 1) {
