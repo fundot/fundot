8ab1af3c42a4530afb6f7ff9344440576bc33833
Jiacheng Huang <huangjiacheng0709@outlook.com>
2020-09-18 19:11:22 +0800

    Create file handling in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
+#include <fstream>
 #include "../include/fundot.h"
+
+using std::cin;
+using std::cout;
+using std::fstream;
 
 using namespace fundot;
 
@@ -7,10 +12,20 @@ int main(int argc, char *argv[])
     if (argc == 1)
     {
         Fundot fdt;
-        fdt.repl(std::cin, std::cout);
+        fdt.repl(cin, cout);
     }
     else if (argc == 2)
     {
+        string file_name(argv[1]);
+        if (file_name.substr(file_name.length() - 3, 3) == ".fd")
+        {
+            fstream fs;
+            fs.open(argv[1]);
+            Object obj;
+            fs >> obj;
+            Fundot fdt;
+            fs.close();
+        }
     }
     return 0;
 }
