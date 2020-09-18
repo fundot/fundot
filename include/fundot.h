1927b83ee602977e243773f87a47a77583dee987
Jiacheng Huang <huangjiacheng0709@outlook.com>
2020-09-18 16:05:09 +0800

    Divide eval() into multiple functions

--- include/fundot.h
+++ include/fundot.h
@@ -23,7 +23,7 @@ namespace fundot
         /**
          * Initialize the object with a map and insert some built-in functions into it.
          */
-        Fundot() : _obj(map<Identifier, Object>()) { _init(); }
+        Fundot();
 
         /**
          * Evaluate an Object as code.
@@ -39,8 +39,42 @@ namespace fundot
         void repl(istream &is, ostream &os);
 
     private:
-        Object _obj;
+        Object _obj; /**< Code stored just as data. */
+        map<Identifier, Object> *_local_scope; /**< Points to the local scope that the code is evaluated. */
+
+        /**
+         * Evaluate a list.
+         * @param obj Object that has type std::list<Object>.
+         */
+        Object _evalList(Object &obj);
+
+        /**
+         * Evaluate an identifier.
+         * @param obj Object that has type Identifier.
+         */
+        Object _evalIdentifier(Object &obj);
+
+        /**
+         * Evaluate a pair.
+         * @param obj Object that has type std::pair<Identifier, Object>.
+         */
+        Object _evalPair(Object &obj);
+
+        /**
+         * Evaluate a vector.
+         * @param obj Object that has type std::vector<Object>.
+         */
+        Object _evalVector(Object &obj);
+
+        /**
+         * Initialize the Object and prepare for evaluation process.
+         */
         void _init();
+        
+        /**
+         * Add a list of numbers.
+         * @param obj Object with numbers to add.
+         */
         Object _add(Object &obj);
     };
 } // namespace fundot
--- src/fundot.cpp
+++ src/fundot.cpp
@@ -2,81 +2,29 @@
 
 namespace fundot
 {
+    Fundot::Fundot() : _obj(map<Identifier, Object>())
+    {
+        _local_scope = &_obj.value<map<Identifier, Object>>();
+        _init();
+    }
+
     Object Fundot::eval(Object &obj)
     {
-        map<Identifier, Object> &obj_map = _obj.value<map<Identifier, Object>>();
         if (obj.holds<list<Object>>())
         {
-            list<Object> &obj_lst = obj.value<list<Object>>();
-            if (obj_lst.front().holds<Identifier>())
-            {
-                Identifier &id = obj_lst.front().value<Identifier>();
-                if (obj_map.count(id) > 0)
-                {
-                    if (id == "quote")
-                    {
-                        return *(++obj_lst.begin());
-                    }
-                    for (list<Object>::iterator it = ++obj_lst.begin(); it != obj_lst.end(); ++it)
-                    {
-                        *it = eval(*it);
-                    }
-                    if (id == "add")
-                    {
-                        return _add(obj);
-                    }
-                    else if (id == "exit")
-                    {
-                        exit(0);
-                    }
-                }
-            }
+            return _evalList(obj);
         }
         else if (obj.holds<Identifier>())
         {
-            Identifier &id = obj.value<Identifier>();
-            size_t dot_pos = id.str().find('.');
-            map<Identifier, Object> *map_ptr = &obj_map;
-            Identifier id_copy = id;
-            while (dot_pos != string::npos)
-            {
-                if (map_ptr->count(id_copy.str().substr(0, dot_pos)) > 0)
-                {
-                    if ((*map_ptr)[id_copy.str().substr(0, dot_pos)].holds<map<Identifier, Object>>())
-                    {
-                        map_ptr = &(*map_ptr)[id_copy.str().substr(0, dot_pos)].value<map<Identifier, Object>>();
-                        id_copy = id_copy.str().substr(dot_pos + 1, id_copy.str().length() - dot_pos - 1);
-                        dot_pos = id_copy.str().find('.');
-                        if (dot_pos == string::npos)
-                        {
-                            return (*map_ptr)[id_copy];
-                        }
-                    }
-                    else
-                    {
-                        return (*map_ptr)[id_copy.str().substr(0, dot_pos)];
-                    }
-                }
-            }
-            if (obj_map.count(id) > 0)
-            {
-                return eval(obj_map[id]);
-            }
+            return _evalIdentifier(obj);
         }
         else if (obj.holds<pair<Identifier, Object>>())
         {
-            pair<Identifier, Object> &obj_pair = obj.value<pair<Identifier, Object>>();
-            obj_pair.second = eval(obj_pair.second);
-            _obj[obj_pair.first] = obj_pair.second;
+            return _evalPair(obj);
         }
         else if (obj.holds<vector<Object>>())
         {
-            vector<Object> &obj_vct = obj.value<vector<Object>>();
-            for (size_t i = 0; i < obj_vct.size(); ++i)
-            {
-                obj_vct[i] = eval(obj_vct[i]);
-            }
-            return obj_vct;
+            return _evalVector(obj);
         }
         return obj;
     }
@@ -92,10 +40,94 @@ namespace fundot
         }
     }
 
+    Object Fundot::_evalList(Object &obj)
+    {
+        list<Object> &obj_lst = obj.value<list<Object>>();
+        if (obj_lst.front().holds<Identifier>())
+        {
+            Identifier &id = obj_lst.front().value<Identifier>();
+            if (_local_scope->count(id) > 0)
+            {
+                if (id == "quote")
+                {
+                    return *(++obj_lst.begin());
+                }
+                for (list<Object>::iterator it = ++obj_lst.begin(); it != obj_lst.end(); ++it)
+                {
+                    *it = eval(*it);
+                }
+                if (id == "add")
+                {
+                    return _add(obj);
+                }
+                else if (id == "exit")
+                {
+                    exit(0);
+                }
+                else if (id == "show")
+                {
+                    return _obj;
+                }
+            }
+        }
+        return obj;
+    }
+
+    Object Fundot::_evalIdentifier(Object &obj)
+    {
+        Identifier &id = obj.value<Identifier>();
+        size_t dot_pos = id.str().find('.');
+        map<Identifier, Object> *map_ptr = _local_scope;
+        Identifier id_copy = id;
+        while (dot_pos != string::npos)
+        {
+            if (map_ptr->count(id_copy.str().substr(0, dot_pos)) > 0)
+            {
+                if ((*map_ptr)[id_copy.str().substr(0, dot_pos)].holds<map<Identifier, Object>>())
+                {
+                    map_ptr = &(*map_ptr)[id_copy.str().substr(0, dot_pos)].value<map<Identifier, Object>>();
+                    id_copy = id_copy.str().substr(dot_pos + 1, id_copy.str().length() - dot_pos - 1);
+                    dot_pos = id_copy.str().find('.');
+                    if (dot_pos == string::npos)
+                    {
+                        return (*map_ptr)[id_copy];
+                    }
+                }
+                else
+                {
+                    return (*map_ptr)[id_copy.str().substr(0, dot_pos)];
+                }
+            }
+        }
+        if (_local_scope->count(id) > 0)
+        {
+            return eval((*_local_scope)[id]);
+        }
+        return obj;
+    }
+
+    Object Fundot::_evalPair(Object &obj)
+    {
+        pair<Identifier, Object> &obj_pair = obj.value<pair<Identifier, Object>>();
+        obj_pair.second = eval(obj_pair.second);
+        _obj[obj_pair.first] = obj_pair.second;
+        return obj;
+    }
+
+    Object Fundot::_evalVector(Object &obj)
+    {
+        vector<Object> &obj_vct = obj.value<vector<Object>>();
+        for (size_t i = 0; i < obj_vct.size(); ++i)
+        {
+            obj_vct[i] = eval(obj_vct[i]);
+        }
+        return obj_vct;
+    }
+
     void Fundot::_init()
     {
         static const map<Identifier, Object> fun_defs = {{Identifier("type"), Identifier("built-in-function")}};
-        static const vector<string> fun_ids = {"quote", "add", "exit"};
+        static const vector<string> fun_ids = {"quote", "add", "exit", "show"};
         for (size_t i = 0; i < fun_ids.size(); ++i)
         {
             _obj[fun_ids[i]] = fun_defs;
